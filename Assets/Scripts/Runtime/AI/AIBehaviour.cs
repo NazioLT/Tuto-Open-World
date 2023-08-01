65e11d8309dfeb0324227dda3288eeed79b3aea8
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2023-08-01 10:34:24 +0200

    AI Behaviour

---
+++ Assets/Scripts/Runtime/AI/AIBehaviour.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehaviour : MonoBehaviour
+{
+    [SerializeField] private AISensor _sensor = null;
+    [SerializeField] private NavMeshAgent _agent = null;
+
+    private void Update()
+    {
+        if(_sensor.CanAttackPlayer)
+        {
+            UpdateAttack();
+            return;
+        }
+
+        if (_sensor.CanSeePlayer)
+        {
+            UpdateHunt();
+            return;
+        }
+
+        UpdateNeutral();
+    }
+
+    private void UpdateAttack()
+    {
+        print("Attacking");
+    }
+
+    private void UpdateHunt()
+    {
+        _agent.isStopped = false;
+        _agent.SetDestination(_sensor.PlayerPosition);
+    }
+
+    private void UpdateNeutral()
+    {
+        _agent.isStopped = true;
+    }
+}
